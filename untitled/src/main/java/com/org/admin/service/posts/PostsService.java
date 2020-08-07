cf2ba3882c058204e08d70305fc77010ec7aad63
yun1112 <wyk1112@hanmail.net>
2020-08-07 16:18:29 +0900

    no message

---
+++ untitled/src/main/java/com/org/admin/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.org.admin.service.posts;
+
+import com.org.admin.domain.posts.PostsRepository;
+import com.org.admin.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
