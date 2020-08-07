0537848492e430c7666bbc36ae1e78fcbcd3da70
yun1112 <wyk1112@hanmail.net>
2020-08-07 16:43:13 +0900

    no message

--- untitled/src/main/java/com/org/admin/service/posts/PostsService.java
+++ untitled/src/main/java/com/org/admin/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.org.admin.service.posts;
 
+import com.org.admin.domain.posts.Posts;
 import com.org.admin.domain.posts.PostsRepository;
+import com.org.admin.web.dto.PostsResponseDto;
 import com.org.admin.web.dto.PostsSaveRequestDto;
+import com.org.admin.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,5 +18,22 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto){
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+        Posts posts=postsRepository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+
+        posts.update(requestDto.getTitle(),requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id){
+        Posts entity=postsRepository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+
+        return new PostsResponseDto(entity);
     }
 }
